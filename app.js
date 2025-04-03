590dfa9a8ae7e65d80c88c59aba68f0ab3806efd
paxhuckstep <paxhuckstep@gmail.com>
2025-04-03 17:04:31 -0600

    mongo installed

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 const { PORT = 4201 } = process.env;
+
+mongoose.connect('mongodb://127.0.0.1:27017/nerdle_db') // change this
+.then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((e) => {
+    console.error(e);
+  });; 
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
