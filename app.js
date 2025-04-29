9736283f7b5e06af3a605b9c8149291b6ce9af52
paxhuckstep <paxhuckstep@gmail.com>
2025-04-29 14:21:25 -0600

    mongo fix

--- app.js
+++ app.js
@@ -15,28 +15,31 @@ const { PORT = 4201 } = process.env;
 //   credentials: true // Enable credentials (cookies, authorization headers, etc.)
 // }));
 
-// app.use(cors({
-//   origin: ['http://localhost:3000', 'https://paxwordle.netlify.app'],
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-//   credentials: true
-// }));
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://paxwordle.netlify.app"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "Accept"],
+    credentials: true,
+  })
+);
 
-// app.use(cors());
-
-app.use(cors({
-  origin: '*',  // temporarily allow all origins for testing
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
+// app.options('/signup', (req, res) => {
+//   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+//   res.header('Access-Control-Allow-Methods', 'POST');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   res.status(200).send();
+// });
 
 app.use(express.json());
-
 app.use("/", mainRouter);
 
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/nerdle_db";
+
 mongoose
-  .connect("mongodb://127.0.0.1:27017/nerdle_db")
+  // .connect("mongodb://127.0.0.1:27017/nerdle_db")
+  .connect(MONGODB_URI)
   .then(() => {
     console.log("Connected to DB");
   })
